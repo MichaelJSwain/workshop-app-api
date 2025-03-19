67ffd2bac83f35b409703753d289edcc928e9c54
MichaelSwain <michaeljswain@gmail.com>
2025-03-19 15:23:54 +0100

    intiial datafile endpoint with dummy data

---
+++ datafile.js
@@ -0,0 +1,25 @@
+const datafile = {
+    projectId: "29170930006",
+    revision: "111",
+    sdkKey: "A52EKCfHmgQrTARNjdwgz",
+    flags:  [{
+        id: Math.random() * 10000,
+        name: "exp-1",
+        type: "a/b",
+        status: "paused"
+      },
+      {
+        id: Math.random() * 10000,
+        name: "exp-2",
+        type: "a/b",
+        status: "paused"
+      },
+      {
+        id: Math.random() * 10000,
+        name: "exp-3",
+        type: "a/b",
+        status: "paused"
+      }]
+}
+
+module.exports = datafile;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const seedData = require('./seedData.js');
 const cors = require('cors');
+const datafile = require('./datafile.js');
 
 app.use(bodyParser.json());
 
@@ -22,6 +23,10 @@ app.post("/api/:projectID/flags", (req, res) => {
     return res.json({message: "ok", data: seedData})
 });
 
+app.get("/cdn/:productID/:sdkKey", (req, res) => {
+    return res.json(datafile);
+});
+
 app.listen("8080", () => {
     console.log("app listening on port 8080");
-})
+});
